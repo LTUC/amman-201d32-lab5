5dc7ebd5203b3e5130db04fbc85b0807caea1888
osama900 <osamaos2233@gmail.com>
2021-06-24 22:32:29 +0300

    problem#6 --stretch goal done

--- app.js
+++ app.js
@@ -189,9 +189,20 @@ let testDynamicArray = [1, 2, 3, 4, 5]; //eslint-disable-line
 
 function multiplyAnyArray(dynamicArray) { //eslint-disable-line
 
+  let multiEx =[1] ;
+  for (let i = 0; i < testDynamicArray.length; i++) {
+
+    //console.log(i);
+    //console.log(testDynamicArray[i]);
+      multiEx = multiply(multiEx[0],testDynamicArray[i]);
+      //console.log('the final example is ' ,multiEx[0]);
+          
+  }
+          return [multiEx[0],'The numbers 1,2,3,4,5 have a product of '+multiEx[0]+'.'];
+
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyAnyArray(testDynamicArray);
+ testMultiplyAnyArray(testDynamicArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
